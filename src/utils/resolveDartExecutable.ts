1ed155134d5c135c97984d4fd4c1722040ab7ad9
Nikita Sirovskiy <nikita.sirovskiy@protonmail.com>
2025-07-11 18:48:36 +0400

    Use the actual executable

--- src/utils/resolveDartExecutable.ts
+++ src/utils/resolveDartExecutable.ts
@@ -1,5 +1,5 @@
 import { existsSync } from "fs";
-import path from "path";
+import * as path from "path";
 import * as vscode from 'vscode';
 import { commandPrefix } from "../extension";
 
@@ -12,8 +12,11 @@ export function resolveDartExecutable(context: vscode.ExtensionContext): string 
 
   const sdk = vscode.workspace.getConfiguration().get<string>(`dart.flutterSdkPath`, '');
 
-  if (sdk.includes('.fvm/versions/')) {
-    return 'fvm exec dart';
+  if (sdk.includes(`.fvm${path.sep}versions${path.sep}`)) {
+    const executableName = process.platform === 'win32' ? 'dart.bat' : 'dart';
+    const fvmDartPath = sdk + `${path.sep}bin${path.sep}${executableName}`;
+
+    return fvmDartPath;
   }
 
   const workspaceRoot =
